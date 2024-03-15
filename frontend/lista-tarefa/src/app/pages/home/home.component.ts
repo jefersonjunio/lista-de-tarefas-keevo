3e2a76be406450f22b714c6fca56d52c60ab5518
jefersonjunio <jjsconstantino@gmail.com>
2024-03-15 14:55:26 -0300

    feat: Adiciona metodo para procurar status

--- frontend/lista-tarefa/src/app/pages/home/home.component.ts
+++ frontend/lista-tarefa/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteTaskComponent } from 'src/app/components/delete-task/delete-task.component';
 import { Task } from 'src/app/models/Task';
@@ -12,13 +13,36 @@ import { TaskService } from 'src/app/services/task.service';
 export class HomeComponent implements OnInit {
   columns = ['Status', 'Descrição', 'Editar', 'Excluir'];
   tasks: Task[] = [];
+  status: Task[] = [];
+  statusForm!: FormGroup
 
   constructor(private taskService: TaskService, public dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.taskService.GetTasks().subscribe((responseTask) => {
        this.tasks = responseTask.data
+       this.status = responseTask.data
     });
+
+    this.statusForm = new FormGroup({
+      status: new FormControl(''),
+    });
+  }
+
+  searchStatus() {
+    const status = this.statusForm.value.status;
+
+    switch (status) {
+      case 'Concluido':
+        this.tasks = this.status.filter(task => task.status === 'Concluido');
+        break;
+      case 'Inicializado':
+        this.tasks = this.status.filter(task => task.status === 'Inicializado');
+        break;
+      default:
+        this.tasks = this.status;
+      break;
+    }
   }
 
   openDialog(id: number) {
