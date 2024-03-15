abaede9d2e8246106f703b467686691ff4a08fd7
jefersonjunio <jjsconstantino@gmail.com>
2024-03-15 10:18:30 -0300

    feat: cria layout page home

--- frontend/lista-tarefa/src/app/app.module.ts
+++ frontend/lista-tarefa/src/app/app.module.ts
@@ -8,6 +8,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -19,7 +25,14 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatTableModule,
+    MatDialogModule,
+    MatIconModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
